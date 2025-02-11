7d456d2d9cca96a93374356ff2e53489a7cf08e9
bcarbis <barry.carbis@sky.com>
2025-02-11 15:05:24 +0000

    updates

--- tests/SwagLabs/selectItem.spec.ts
+++ tests/SwagLabs/selectItem.spec.ts
@@ -4,14 +4,13 @@ import { pauseExecution } from '../utils';
 import { addStep } from '../allureActions';
 
 test('select single item test', async ({ page }) => {
-    // Run the login script
+
    await addStep('Login to SauceDemo', async () => {
     await loginToSaucedemo(page);
    });
 
     let firstSelection: string;
 
-    // Select Single Item
  await addStep('Select First Item', async () => {
         await pauseExecution(1000);
         firstSelection = await selectFirstItem(page);
